e8942bc1c251bfb16ba549fd3fc2ac0b72ee1989
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2018-06-18 22:52:05 +0200

    Bundles the language server
    This adds the LS binary directly into the compiled jar and thus removes
    the need to have it installed on the system
    
    - updates README.adoc with the new changes

--- src/com/vogella/lsp/flutter/FlutterLanguageServerStreamProvider.java
+++ src/com/vogella/lsp/flutter/FlutterLanguageServerStreamProvider.java
@@ -1,8 +1,11 @@
 package com.vogella.lsp.flutter;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
 
@@ -10,9 +13,14 @@ public class FlutterLanguageServerStreamProvider extends ProcessStreamConnection
 
 
 	public FlutterLanguageServerStreamProvider() {
-		super(
-			Arrays.asList("/bin/sh", "/home/jhungershausen/.pub-cache/bin/dart_language_server"),
-			new File(".").getAbsolutePath());
+		try {
+			URL url = FileLocator.toFileURL(getClass().getResource("/language_server/bin/dart_language_server.dart.snapshot"));
+			setCommands(Arrays.asList("/usr/lib/dart/bin/dart", new File(url.getPath()).getAbsolutePath()));
+			setWorkingDirectory(System.getProperty("user.dir"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
