13d897caa1f4b7e8ac46617c99a7e0ef41a663f7
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-01-22 14:12:11 +0100

    Rename PathLocator to CommandLineTools

--- com.vogella.eclipsedart/src/com/vogella/eclipsedart/CommandLineTools.java
+++ com.vogella.eclipsedart/src/com/vogella/eclipsedart/CommandLineTools.java
@@ -1,37 +1,35 @@
 package com.vogella.eclipsedart;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Optional;
 
 import org.apache.commons.io.IOUtils;
+import org.eclipse.swt.program.Program;
 
-public class PathLocator {
+public class CommandLineTools {
 
 	public static Optional<String> getDartSDKLocation() {
 		return getLocation("dart");
 	}
 
-	public static Optional<String> getLanguageServerLocation() {
-		return Optional.of(".pub-cache/bin/dart_language_server");
-	}
-
-	public static Optional<String> getPubLocation() {
-		return getLocation("pub");
-	}
-
 	protected static Optional<String> getLocation(String program) {
-		String location = null;
-		String[] commands = new String[] { "/usr/bin/zsh", "-c", "which " + program };
+		String[] commands = new String[] { "/usr/bin/sh", "-c", "which " + program };
 		//TODO: Add support for Windows and MacOS
-
+		
+		return execute(commands);
+	}
+	
+	public static Optional<String> execute(String... commands) {
 		BufferedReader reader = null;
 		try {
 			Process process = Runtime.getRuntime().exec(commands);
 			reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			location = reader.readLine();
-			return Optional.of(location);
+			String output = reader.readLine();
+
+			return Optional.ofNullable(output);
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		} finally {
@@ -39,6 +37,7 @@ public class PathLocator {
 		}
 
 		return Optional.empty();
+		
 	}
 
 }
