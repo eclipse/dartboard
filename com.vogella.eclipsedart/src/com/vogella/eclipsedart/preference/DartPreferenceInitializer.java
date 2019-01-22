0cfb916d384c76413e9a1ce9ea6b2abc9821cf45
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-01-22 14:29:03 +0100

    Use InstanceScope when initializing the preferences

--- com.vogella.eclipsedart/src/com/vogella/eclipsedart/preference/DartPreferenceInitializer.java
+++ com.vogella.eclipsedart/src/com/vogella/eclipsedart/preference/DartPreferenceInitializer.java
@@ -1,17 +1,17 @@
 package com.vogella.eclipsedart.preference;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 
-import com.vogella.eclipsedart.Constants;
 import com.vogella.eclipsedart.CommandLineTools;
+import com.vogella.eclipsedart.Constants;
 
 public class DartPreferenceInitializer extends AbstractPreferenceInitializer {
 
 	@Override
 	public void initializeDefaultPreferences() {
-		IEclipsePreferences preferences = DefaultScope.INSTANCE.getNode(Constants.PREFERENCES_KEY);
+		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Constants.PREFERENCES_KEY);
 
 		CommandLineTools.getDartSDKLocation().ifPresent(location -> {
 			preferences.put(Constants.PREFERENCES_SDK_LOCATION, location);
