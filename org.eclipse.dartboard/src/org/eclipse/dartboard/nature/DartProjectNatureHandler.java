cf921e78367d3e22899e03602fbbc84ca358b2e7
Lakshminarayana Nekkanti <39690425+lak-proddev@users.noreply.github.com>
2019-06-30 22:40:57 +0530

    Add "New Dart file" & "New Dart project" to "New" context menu (#62)
    closes #6 
    
    Signed-off-by: Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/nature/DartProjectNatureHandler.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/nature/DartProjectNatureHandler.java
@@ -17,16 +17,12 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdapterManager;
-import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.dartboard.Constants;
+import org.eclipse.dartboard.util.ProjectUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -56,28 +52,7 @@ public class DartProjectNatureHandler extends AbstractHandler {
 			if (resourceAdapter != null) {
 				IResource resource = resourceAdapter;
 				IProject project = resource.getProject();
-				try {
-					IProjectDescription description = project.getDescription();
-					String[] natures = description.getNatureIds();
-					String[] newNatures = new String[natures.length + 1];
-					System.arraycopy(natures, 0, newNatures, 0, natures.length);
-
-					newNatures[natures.length] = Constants.NATURE_ID;
-
-					// validate the natures
-					IWorkspace workspace = ResourcesPlugin.getWorkspace();
-					IStatus status = workspace.validateNatureSet(newNatures);
-
-					// only apply new nature, if the status is ok
-					if (status.getCode() == IStatus.OK) {
-						description.setNatureIds(newNatures);
-						project.setDescription(description, null);
-					}
-
-					return status;
-				} catch (CoreException e) {
-					throw new ExecutionException(e.getMessage(), e);
-				}
+				return ProjectUtil.addProjectNature(project, new NullProgressMonitor());
 			}
 		}
 
