00a495e1aaa6b51a21c69641680352a22543c907
Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>
2019-07-09 14:08:24 +0530

    Auto-detect Dart SDK in preference page does not work for existing workspace #83
    Signed-off-by: Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartPreferencePage.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartPreferencePage.java
@@ -21,6 +21,7 @@ import java.nio.file.Paths;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.dartboard.Constants;
 import org.eclipse.dartboard.Messages;
+import org.eclipse.dartboard.util.StringUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditor;
@@ -104,6 +105,9 @@ public class DartPreferencePage extends FieldEditorPreferencePage implements IWo
 	 * @return
 	 */
 	private Path getPath(String location) {
+		if (StringUtil.isNullOrEmpty(location)) {
+			return null;
+		}
 		Path path = Paths.get(location);
 		if (Files.exists(path)) {
 			try {
