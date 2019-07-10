ec3ab12bca463ca36265b2aa734cd16b3cb69f1a
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-07-10 21:10:33 +0200

    Fix: NPE in Run as Dart Program
    fixes #94
    
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/launch/LaunchShortcut.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/launch/LaunchShortcut.java
@@ -17,6 +17,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.dartboard.Constants;
 import org.eclipse.dartboard.Messages;
@@ -42,14 +43,41 @@ public class LaunchShortcut implements ILaunchShortcut {
 
 	@Override
 	public void launch(ISelection selection, String mode) {
+		// Get the active project from the
 		IProject selected = null;
 		if (selection instanceof StructuredSelection) {
 			Object firstElement = ((StructuredSelection) selection).getFirstElement();
-			if (firstElement instanceof IProject) {
-				selected = (IProject) firstElement;
+			if (firstElement instanceof IResource) {
+				selected = ((IResource) firstElement).getProject();
 			}
 		}
 
+		// Shouldn't happen
+		if (selected == null) {
+			MessageDialog.openError(null, Messages.Launch_NoConfigurationFound_Title,
+					Messages.Launch_NoConfigurationFound_Body);
+			return;
+		}
+
+		launchProject(selected, mode);
+	}
+
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+		IResource resource = editor.getEditorInput().getAdapter(IResource.class);
+
+		if (resource == null) {
+			MessageDialog.openError(null, Messages.Launch_NoConfigurationFound_Title,
+					Messages.Launch_NoConfigurationFound_Body);
+			return;
+		}
+
+		IProject project = resource.getProject();
+
+		launchProject(project, mode);
+	}
+
+	private void launchProject(IProject project, String mode) {
 		ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
 
 		ILaunchConfigurationType type = manager.getLaunchConfigurationType(Constants.LAUNCH_CONFIGURATION_ID);
@@ -57,9 +85,10 @@ public class LaunchShortcut implements ILaunchShortcut {
 		Set<String> modes = new HashSet<>();
 		modes.add(mode);
 		try {
+			// Find first launch configuration for selected project.
 			ILaunchConfiguration launchConfiguration = null;
 			for (ILaunchConfiguration conf : manager.getLaunchConfigurations(type)) {
-				if (conf.getAttribute(Constants.LAUNCH_SELECTED_PROJECT, "").equalsIgnoreCase(selected.getName())) { //$NON-NLS-1$
+				if (conf.getAttribute(Constants.LAUNCH_SELECTED_PROJECT, "").equalsIgnoreCase(project.getName())) { //$NON-NLS-1$
 					launchConfiguration = conf;
 				}
 			}
@@ -82,13 +111,5 @@ public class LaunchShortcut implements ILaunchShortcut {
 
 		// Open LaunchConfig Manager
 		// Precreate a new launch config with the selected project preselected
-
-	}
-
-	@Override
-	public void launch(IEditorPart editor, String mode) {
-		// TODO: Can't seem to trigger this part. If I understand correctly, this is
-		// triggered when a launch config is triggered from an active editor. But
-		// somehow the option is not shown for dart editors.
 	}
 }
