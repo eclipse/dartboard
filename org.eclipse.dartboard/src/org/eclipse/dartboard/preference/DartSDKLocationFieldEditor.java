63fbddbf33dcd899201578fc2e609253cb4358df
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-07-12 21:05:30 +0200

    Adds better checks
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartSDKLocationFieldEditor.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartSDKLocationFieldEditor.java
@@ -12,8 +12,12 @@ import org.eclipse.dartboard.Messages;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DartSDKLocationFieldEditor extends DirectoryFieldEditor {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DartSDKLocationFieldEditor.class);
 
 	public DartSDKLocationFieldEditor(String preferencesKey, String label, Composite parent) {
 		super(preferencesKey, label, parent);
@@ -34,15 +38,32 @@ public class DartSDKLocationFieldEditor extends DirectoryFieldEditor {
 	@SuppressWarnings("nls")
 	private boolean isValidDartSDK(String location) {
 		Path path = Paths.get(location);
+		// If the entered file doesn't exist, there is no need to run it
+		// Similarly if the file is not a directory
 		if (!Files.exists(path)) {
 			return false;
+		}
+		try {
+			path = path.toRealPath();
+		} catch (IOException e1) {
+			LOG.error("Couldn't follow symlink", e1);
+			return false;
+		}
+
+		String executablePath = null;
+		if (path.endsWith("bin/dart")) {
+			executablePath = path.toAbsolutePath().toString();
+		} else if (path.endsWith("bin")) {
+			executablePath = path.toAbsolutePath().toString() + "/dart";
+		} else {
+			executablePath = path.toAbsolutePath().toString() + "/bin/dart";
 		}
 
 		String[] commands;
 		if (Platform.OS_WIN32.equals(Platform.getOS())) {
-			commands = new String[] { "cmd", "/c", location + "/bin/dart --version" };
+			commands = new String[] { "cmd", "/c", executablePath + " --version" };
 		} else {
-			commands = new String[] { "/bin/bash", "-c", location + "/bin/dart --version" };
+			commands = new String[] { "/bin/bash", "-c", executablePath + " --version" };
 		}
 
 		ProcessBuilder processBuilder = new ProcessBuilder(commands);
