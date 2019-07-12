ebbb093c458840e3785bb53358f558dff746d270
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-07-12 20:16:40 +0200

    Add check for valid Dart SDK to preference page
    closes #83
    
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartSDKLocationFieldEditor.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/preference/DartSDKLocationFieldEditor.java
@@ -1,7 +1,13 @@
 package org.eclipse.dartboard.preference;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.dartboard.Messages;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.swt.events.ModifyListener;
@@ -17,13 +23,40 @@ public class DartSDKLocationFieldEditor extends DirectoryFieldEditor {
 	@Override
 	protected boolean doCheckState() {
 		String location = getTextControl().getText();
-		File file = new File(location);
-		boolean isValid = file.exists();
+		boolean isValid = isValidDartSDK(location);
 		if (!isValid) {
 			setErrorMessage(Messages.Preference_SDKNotFound_Message);
 			showErrorMessage();
 		}
 		return isValid;
+	}
+
+	@SuppressWarnings("nls")
+	private boolean isValidDartSDK(String location) {
+		Path path = Paths.get(location);
+		if (!Files.exists(path)) {
+			return false;
+		}
+
+		String[] commands;
+		if (Platform.OS_WIN32.equals(Platform.getOS())) {
+			commands = new String[] { "cmd", "/c", location + "/bin/dart --version" };
+		} else {
+			commands = new String[] { "/bin/bash", "-c", location + "/bin/dart --version" };
+		}
+
+		ProcessBuilder processBuilder = new ProcessBuilder(commands);
+
+		processBuilder.redirectErrorStream(true);
+		String version = null;
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(processBuilder.start().getInputStream()))) {
+			version = reader.readLine();
+		} catch (IOException e) {
+			return false;
+		}
+
+		return version.startsWith("Dart VM version");
 	}
 
 	protected void addModifyListener(ModifyListener listener) {
