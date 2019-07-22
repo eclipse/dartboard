94be33820b9a728f9d2a2cafbb477283fb2b96a8
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-07-22 21:49:39 +0200

    Use OSGI service instead of StartupListener
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/ListenerService.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/ListenerService.java
@@ -3,12 +3,14 @@ package org.eclipse.dartboard;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.dartboard.pub.PubspecChangeListener;
-import org.eclipse.ui.IStartup;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
 
-public class StartupListener implements IStartup {
+@Component(immediate = true)
+public class ListenerService {
 
-	@Override
-	public void earlyStartup() {
+	@Activate
+	public void registerListeners() {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		workspace.addResourceChangeListener(new PubspecChangeListener());
 	}
