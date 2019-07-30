2aeb5f13b6d45ebb77b47abfae0a8cbd6a973ead
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-07-30 17:57:07 +0200

    Remove obsolete object reference
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/ListenerService.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/ListenerService.java
@@ -13,7 +13,6 @@
  *******************************************************************************/
 package org.eclipse.dartboard;
 
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.dartboard.pub.PubspecChangeListener;
 import org.osgi.service.component.annotations.Activate;
@@ -24,8 +23,7 @@ public class ListenerService {
 
 	@Activate
 	public void registerListeners() {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		workspace.addResourceChangeListener(new PubspecChangeListener());
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(new PubspecChangeListener());
 	}
 
 }
