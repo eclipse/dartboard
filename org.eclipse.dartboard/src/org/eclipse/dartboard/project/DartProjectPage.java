e06216c5b68f02344aa16d0c311a9fe0a07f84c6
Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>
2019-08-06 18:55:37 +0530

    [Project Wizard] Template loading should use wizard progress bar #118
    Signed-off-by: Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>

--- org.eclipse.dartboard/src/org/eclipse/dartboard/project/DartProjectPage.java
+++ org.eclipse.dartboard/src/org/eclipse/dartboard/project/DartProjectPage.java
@@ -13,16 +13,16 @@
  *******************************************************************************/
 package org.eclipse.dartboard.project;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.dartboard.Constants;
 import org.eclipse.dartboard.Messages;
 import org.eclipse.dartboard.stagehand.StagehandService;
 import org.eclipse.dartboard.stagehand.StagehandTemplate;
 import org.eclipse.jface.dialogs.IMessageProvider;
-import org.eclipse.jface.dialogs.ProgressIndicator;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -37,13 +37,17 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DartProjectPage extends WizardNewProjectCreationPage {
+	private static final Logger LOG = LoggerFactory.getLogger(DartProjectPage.class);
 
 	private ScopedPreferenceStore preferences;
 	private Combo stagehandTemplates;
 	private Button useStagehandButton;
 	private List<StagehandTemplate> templates;
+	private boolean initialized;
 
 	public DartProjectPage(String pageName) {
 		super(pageName);
@@ -80,7 +84,6 @@ public class DartProjectPage extends WizardNewProjectCreationPage {
 
 		useStagehandButton = new Button(stagehandGroup, SWT.CHECK);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(useStagehandButton);
-		useStagehandButton.setEnabled(false);
 
 		stagehandTemplates = new Combo(stagehandGroup, SWT.READ_ONLY);
 		stagehandTemplates.setEnabled(useStagehandButton.getSelection());
@@ -88,30 +91,41 @@ public class DartProjectPage extends WizardNewProjectCreationPage {
 
 		useStagehandButton.setText(Messages.NewProject_Stagehand_UseStagehandButtonText);
 		useStagehandButton.addSelectionListener(new SelectionAdapter() {
+
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				// Enable/Disable the stagehand templates list if the button is
-				// checked/unchecked
-				stagehandTemplates.setEnabled(useStagehandButton.getSelection());
-				if (stagehandTemplates.getSelectionIndex() == -1) {
-					stagehandTemplates.select(0);
+			public void widgetSelected(SelectionEvent event) {
+				boolean selected = useStagehandButton.getSelection();
+				stagehandTemplates.setEnabled(selected);
+				if (selected) {
+					if (!initialized) {
+						try {
+							getWizard().getContainer().run(true, true, monitor -> {
+								try {
+									monitor.beginTask(Messages.NewProject_Stagehand_FetchStagehand,
+											IProgressMonitor.UNKNOWN);
+									templates = StagehandService.getStagehandTemplates();
+									Display.getDefault().asyncExec(() -> {
+										templates.forEach(str -> stagehandTemplates.add(str.getDisplayName()));
+										initialized = true;
+									});
+								} finally {
+									monitor.done();
+								}
+							});
+						} catch (InvocationTargetException | InterruptedException e) {
+							initialized = false;
+							stagehandTemplates.setEnabled(false);
+							stagehandTemplates.removeAll();
+							useStagehandButton.setSelection(false);
+							LOG.error(e.getMessage());
+						}
+					}
+					if (stagehandTemplates.getSelectionIndex() == -1) {
+						stagehandTemplates.select(0);
+					}
 				}
 			}
 		});
-
-		ProgressIndicator indicator = new ProgressIndicator(stagehandGroup);
-		GridDataFactory.fillDefaults().grab(true, false).applyTo(indicator);
-		indicator.beginAnimatedTask();
-
-		Job.create(Messages.NewProject_Stagehand_FetchStagehand, monitor -> {
-			templates = StagehandService.getStagehandTemplates();
-
-			Display.getDefault().asyncExec(() -> {
-				templates.forEach(str -> stagehandTemplates.add(str.getDisplayName()));
-				indicator.done();
-				useStagehandButton.setEnabled(true);
-			});
-		}).schedule();
 	}
 
 	public StagehandTemplate getGenerator() {
