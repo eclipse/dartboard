8768b9987f177577e4bd7d183382f54d5b4aed15
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-11-11 12:11:16 +0100

    Add property tester for flutter projects
    Signed-off-by: Jonas Hungershausen <jonas.hungershausen@vogella.com>

---
+++ org.eclipse.dartboard.flutter/src/org/eclipse/dartboard/flutter/launch/LaunchShortcut.java
@@ -0,0 +1,18 @@
+package org.eclipse.dartboard.flutter.launch;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+public class LaunchShortcut implements ILaunchShortcut {
+
+	@Override
+	public void launch(ISelection selection, String mode) {
+	
+	}
+
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+	}
+
+}
